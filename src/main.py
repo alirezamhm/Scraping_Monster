027e974980484a49d1bf8e318080b9bbf23832db
alirezamhm <alireza.am1379@gmail.com>
2020-08-21 17:25:25 +0430

    get results title and name and location

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Iran'
 page = requests.get(URL)
@@ -8,10 +9,12 @@ soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='ResultsContainer')
 job_elements = results.find_all('section', class_='card-content')
 for element in job_elements:
-    title = element.find_all('h2', class_='title')
-    company = element.find_all('div', class_='company')
-    location = element.find_all('div', class_='location')
-    print(title)
-    print(company)
-    company = element.find_all('div', classmethod)
-    print(location, end='\n'*2)
+    title = element.find('h2', class_='title')
+    company = element.find('div', class_='company')
+    location = element.find('div', class_='location')
+    if not (title and company and location):
+        continue
+    print(title.text.strip())
+    print(company.text.strip())
+    print(location.text.strip())
+    print()
