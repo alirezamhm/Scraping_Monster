d0a47eb38abfa8f21d3c415f5a805e999f30cbec
alirezamhm <alireza.am1379@gmail.com>
2020-08-21 16:42:02 +0430

    get all of the search  resutls and seperate them

--- src/main.py
+++ src/main.py
@@ -5,3 +5,13 @@ URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Iran'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='ResultsContainer')
+job_elements = results.find_all('section', class_='card-content')
+for element in job_elements:
+    title = element.find_all('h2', class_='title')
+    company = element.find_all('div', class_='company')
+    location = element.find_all('div', class_='location')
+    print(title)
+    print(company)
+    company = element.find_all('div', classmethod)
+    print(location, end='\n'*2)
