3b6ab9ea3f9d2bfa5c3507178e2b1aff7268c777
alirezamhm <alireza.am1379@gmail.com>
2020-08-21 16:26:34 +0430

    src file added

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Iran'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
